8ebe6e2cb6dacdeff6e91c715219810bb994f2f3
Leandro Parrado <lparrdev@gmail.com>
2022-06-02 15:15:05 -0400

    editing maleCharBase.src functionality and added new function getFilename

--- script.js
+++ script.js
@@ -1,21 +1,27 @@
 window.onload = function () {
 
-    var maleCharBase = new Image()
-    var malecharBaseNum = Math.floor(Math.random() * 1) + 1 // Change according to # of base files
-    var maleCharBaseName = "malebase" + malecharBaseNum + ".png"
-    maleCharBase.src = maleCharBaseName
+    function getFilename(fullPath) {
+        return fullPath.replace(/^.*[\\\/]/, '');
+    }
 
-    maleCharBase.onload=function()
-    {
+    // WIP
+    var maleCharBase = new Image()
+    var malecharBaseNum = Math.floor(Math.random() * 5) + 1 // Change according to # of base files
+    var maleCharBaseName = getFilename("Assets/maleChar/base/malebase" + malecharBaseNum + ".png")
+    // WIP
+    console.log(maleCharBaseName)
+    maleCharBase.src = getFilename(maleCharBaseName)
+    console.log(maleCharBaseName.src)
+
+    maleCharBase.onload = function () {
         buildMaleChar();
     }
 
-    function buildMaleChar()
-    {
+    function buildMaleChar() {
         var canvas = document.getElementById('canvas')
         var ctx = canvas.getContext('2d')
-        canvas.width=512;
-        canvas.height=512;
+        canvas.width = 512;
+        canvas.height = 512;
 
         //Necessary if parts need to be adjusted if files are not centered
         // ctx.drawImage(maleCharBase, ((512-maleCharBase.width)/2), 50)
