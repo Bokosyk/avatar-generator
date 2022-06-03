5540fb2e03f774ff5947a3f9846802e514f6e4b8
Leandro Parrado <lparrdev@gmail.com>
2022-06-02 22:39:36 -0400

    Added views folder for app.engine, ejs dependency added

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
+// Dependencies
 const express = require('express')
-
-// Console Emoji
 const emoji = String.fromCodePoint(0x1F30D)
 const app = express()
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// to map the EJS template engine to “.html” files
+app.engine('html', require('ejs').renderFile);
+
 var PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get('/', function (req, res) {
+  res.render("index.html")
+});
 
-app.listen(PORT, () => {
+app.listen(PORT, function(err){
+  if (err) console.log(err);
   console.log( "Server listening on ==> " + emoji + " http://localhost:" + PORT);
 })
